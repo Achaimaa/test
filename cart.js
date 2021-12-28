aad3abf978dd7d02197ea0b4e8003f92b43cf6bb
Achaimaa <71615152+Achaimaa@users.noreply.github.com>
2021-12-28 19:26:53 +0200

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const cartSchema = mongoose.Schema({
+  
+    ownedByString:
+    {
+      type: String,
+    },
+    items: 
+    [{
+      productid: {type: String},
+       // required: true,
+
+        quantity:{type: Number,
+        default: 1   
+        }
+    }]
+ 
+})
+
+exports.Cart = mongoose.model('Cart', cartSchema);
